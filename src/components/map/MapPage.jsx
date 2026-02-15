315dde2143f77be5b1280c75b1bea1f13589b646
Jack Gardner <jtgardner99@gmail.com>
2026-02-15 05:14:06 -0600

    initial commit

--- src/components/map/MapPage.jsx
+++ src/components/map/MapPage.jsx
@@ -1,20 +1,37 @@
 import { Marker, Popup } from 'react-leaflet'
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
+import 'leaflet/dist/leaflet.css'
 import { useMap } from 'react-leaflet/hooks'
+import { useEffect, useState } from 'react'
+import { getVenues } from '../../services/venuesService'
 
 export const MapPage = () => {
+    const [venues, setVenues] = useState([])
+
+    useEffect(() => {
+        getVenues().then(setVenues)
+    }, [])
+
     return (
-        <MapContainer className="leaflet-container" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+        <div>
+            <MapContainer className="leaflet-container" center={[36.1627, -86.7816]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {venues.map((venue) => {
+                    return (
+                    <Marker position={venue.geoCode}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                    )
+                })}
+                
             </MapContainer>
+        </div>
+        
     )
 }
