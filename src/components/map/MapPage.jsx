5d1fb1c009f15324f7208816f03b942294cb9559
Jack Gardner <jtgardner99@gmail.com>
2026-02-23 11:23:06 -0600

    initial commit

--- src/components/map/MapPage.jsx
+++ src/components/map/MapPage.jsx
@@ -3,10 +3,11 @@ import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import 'leaflet/dist/leaflet.css'
 import { useMap } from 'react-leaflet/hooks'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getArtistShows } from '../../services/artistShowsService'
 import { Link } from 'react-router-dom'
 import { getGenres } from '../../services/genreService'
+import { createPortal } from 'react-dom'
 
 export const MapPage = () => {
     const [artistShows, setArtistShows] = useState([])
@@ -17,6 +18,8 @@ export const MapPage = () => {
     const [filteredShows, setFilteredShows] = useState([])
     const [search, setSearch] = useState("")
     const [eventIsVisible, setEventIsVisible] = useState(false)
+    const [selectedShow, setSelectedShow] = useState(null)
+    const mapContainerRef = useRef(null)
 
     useEffect(() => {
         getArtistShows().then(setArtistShows)
@@ -33,8 +36,15 @@ export const MapPage = () => {
             shows = shows.filter((show) => show.artist.artistName.toLowerCase().includes(search.toLowerCase()))
         }
 
-        setFilteredShows(shows)
-    }, [artistShows, genre, search])
+        if(!eventIsVisible) {
+            const timer = setTimeout(() => {
+                setFilteredShows(shows)
+            }, 300)
+
+            return () => clearTimeout(timer)
+        }
+
+    }, [artistShows, genre, search, eventIsVisible])
 
 
 
@@ -53,8 +63,9 @@ export const MapPage = () => {
         })
     }
 
-    const handleEventVisible = () => {
+    const handleEventVisible = (show) => {
         setEventIsVisible(true)
+        setSelectedShow(show)
     }
 
     const handleEventInvisible = () => {
@@ -81,7 +92,27 @@ export const MapPage = () => {
                     }} />
                 </div>
             </div>
-            <div className='showcase-main-map'>
+            <div className='showcase-main-map' ref={mapContainerRef}>
+                {eventIsVisible && <div className='map-blocker' onClick={handleEventInvisible} />}
+
+                {mapContainerRef.current && createPortal(    
+                        <div className={`popup-overlay ${eventIsVisible ? 'active' : ''}`}>
+                                    {selectedShow && (
+                                        <div>
+                                        <h2>{selectedShow.eventTitle}</h2>
+                                        <div>{selectedShow.artist?.artistName}</div>
+                                        <div>{formatDateTime(selectedShow.dateTime)}</div>
+                                        <div>@{selectedShow.venue?.venueName}</div>
+                                        <div>
+                                            <button>Get Tickets</button>
+                                        </div>
+                                        <button className='close-btn' onClick={handleEventInvisible}>X</button>
+                                    </div>  
+                                    )}
+                                </div>,
+                        mapContainerRef.current
+                    )
+                }
                 <MapContainer center={[36.1627, -86.7816]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -90,21 +121,13 @@ export const MapPage = () => {
                 {filteredShows.map((show) => {
                     return (
                         <>
-                    <Marker position={[show.venue?.lat, show.venue?.lng]}>                    
-                                <div className={`popup-overlay ${eventIsVisible ? 'active' : ''}`}>
-                                    <div>
-                                        <h2>{show.eventTitle}</h2>
-                                        <div>{show.artist?.artistName}</div>
-                                        <div>{formatDateTime(show.dateTime)}</div>
-                                        <div>@{show.venue?.venueName}</div>
-                                        <button className='close-btn' onClick={handleEventInvisible}>X</button>
-                                    </div>
-                                    
-                                </div>
+                    <Marker position={[show.venue?.lat, show.venue?.lng]}>
                                 <Popup>
                                     <div className='logo-text'>{show.eventTitle}</div>
                                     <div>{formatDateTime(show.dateTime)}</div>
-                                    <button onClick={handleEventVisible}>Event Details</button>
+                                    <button onClick={() => {
+                                        handleEventVisible(show)
+                                    }}>Event Details</button>
                                 </Popup>                                
                 </Marker>
                     </>
