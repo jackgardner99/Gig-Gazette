9dd11fc357c0d8ca4b9312ba4b5a657815217f22
Jack Gardner <jtgardner99@gmail.com>
2026-06-12 21:11:45 -0500

    event details

--- src/components/details/EventDetails.jsx
+++ src/components/details/EventDetails.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { getShowById } from "../../services/artistShowsService"
 import { getOpenMicById, getWritersRoundById } from "../../services/eventService"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const CONFIG = {
     show: {
@@ -28,5 +28,41 @@ export const EventDetails = () => {
     const navigate = useNavigate()
     const [details, setDetails] = useState(null)
 
-    return <></>
+    useEffect(() => {
+        CONFIG.getFn(id).then(data => {
+            setDetails({
+                event_title: data.event_title ?? '',
+                date: data.date ?? '',
+                start_time: data.start_time ?? '',
+                end_time: data.end_time ?? '',
+                description: data.description ?? ''
+            })
+        })
+    }, [id])
+
+    
+
+    return (
+        <div className="page-content">
+            <div>
+                <button
+                    onClick={() => {
+                        navigate("/")
+                    }}
+                >Go Back</button>
+            </div>
+            <div>
+                {details.event_title}
+            </div>
+            <div>
+                {details.date}
+            </div>
+            <div>
+                {details.start_time} - {details.end_time}
+            </div>
+            <div>
+                {details.description}
+            </div>
+        </div>
+    )
 }
